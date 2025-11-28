fe97fa8532b9335a523ebdb8cbeb9c113dd299f6
Mohamed Asem <asemm9930@gmail.com>
2025-11-28 15:54:46 +0200

    handle unhandled rejections and uncaught exceptions

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+// handle uncaught exceptions:
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('uncaught exception .. shutting down ...');
+  process.exit(1);
+});
+
 import dotenv from 'dotenv';
 dotenv.config();
 import app from './app.js';
 
-app.listen(process.env.PORt, () =>
-  console.log(`app listen on port ${process.env.PORt}`)
+const server = app.listen(process.env.PORT, () =>
+  console.log(`app listen on port ${process.env.PORT}`)
 );
+
+// handle unhandled rejections
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('unhandled rejections. shutting down ...');
+  server.close(() => {
+    process.exit(1);
+  });
+});
